dc7993bb80570779c85d55b949902a914a1f2ae0
Undolskiy Alexey <alexey.undolskiy@spbpu.com>
2023-04-20 22:07:13 +0300

    Hotfix Updated tests

--- tests/test_blockchain.py
+++ tests/test_blockchain.py
@@ -1,22 +1,17 @@
 import pytest
-import threading
-from time import sleep
-from src.node import Node
-from src.main import start_blockchain
+from node import Node
+from main import Starter
 
 
 @pytest.mark.parametrize("node_id, node_address, node2_address, node3_address, make_genesis",
                          [(1, "127.0.0.1:10000", "127.0.0.1:10001", "127.0.0.1:10002", True)])
 def test_blockchain_init(node_id, node_address, node2_address, node3_address, make_genesis):
+    n_blocks = 10
+
     node = Node(node_id)
     assert node is not None
-    thread = threading.Thread(target=start_blockchain,
-                              args=(node, node_address, node2_address, node3_address, make_genesis), daemon=False)
-    thread.start()
-    i = 0
-    while len(node.blockchain) == 0 and i < 50:
-        sleep(0.1)
-        i += 1
+    starter = Starter(node, node_address, node2_address, node3_address, make_genesis)
+    starter.start_blockchain(n_blocks)
     assert len(node.blockchain) != 0
     first_block = node.blockchain[0]
     assert first_block.index == 1
@@ -24,25 +19,22 @@ def test_blockchain_init(node_id, node_address, node2_address, node3_address, ma
     assert first_block.node_id == node.id
 
 
-@pytest.mark.parametrize("node_id, node_address, node2_address, node3_address, make_genesis",
-                         [(1, "127.0.0.1:10000", "127.0.0.1:10001", "127.0.0.1:10002", True)])
-def test_blockchain(node_id, node_address, node2_address, node3_address, make_genesis):
-    node1 = Node(1)
-    node2 = Node(2)
-    node3 = Node(3)
-    thread1 = threading.Thread(target=start_blockchain, args=(node1, node_address, node2_address, node3_address, False),
-                               daemon=False)
-    thread2 = threading.Thread(target=start_blockchain, args=(node2, node2_address, node_address, node3_address, False),
-                               daemon=False)
-    thread3 = threading.Thread(target=start_blockchain, args=(node3, node3_address, node_address, node2_address, True),
-                               daemon=False)
-    thread1.start()
-    thread2.start()
-    thread3.start()
+@pytest.mark.parametrize("node_id, node2_id, node3_id, node_address, node2_address, node3_address",
+                         [(1, 2, 3, "127.0.0.1:10000", "127.0.0.1:10001", "127.0.0.1:10002")])
+def test_blockchain(node_id, node2_id, node3_id, node_address, node2_address, node3_address):
+    n_blocks = 10
 
-    n = 30
-    while len(node1.blockchain) < n or len(node2.blockchain) < n or len(node3.blockchain) < n:
-        sleep(0.1)
+    node1 = Node(node_id)
+    node2 = Node(node2_id)
+    node3 = Node(node3_id)
 
-    for i in range(n):
-        assert node1.blockchain[i] == node2.blockchain[i] == node3.blockchain[i]
+    starter1 = Starter(node1, node_address, node2_address, node3_address, False)
+    starter2 = Starter(node2, node2_address, node_address, node3_address, False)
+    starter3 = Starter(node3, node_address, node3_address, node_address, True)
+
+    starter1.start_message_receiver()
+    starter2.start_message_receiver()
+    starter3.start_blockchain(n_blocks)
+
+    for i in range(0, n_blocks):
+        assert starter1.node.blockchain[i] == starter2.node.blockchain[i] == starter3.node.blockchain[i]
